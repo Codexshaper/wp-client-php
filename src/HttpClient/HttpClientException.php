b99a2c3ece82b8854d7e1aa2e88adc6c02f76b07
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-05-13 02:10:43 +0600

    Initialize Wp API Client for PHP

---
+++ src/HttpClient/HttpClientException.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * WP REST API HTTP Client Exception
+ *
+ * @category HttpClient
+ * @package  Codexshaper/WP
+ */
+
+namespace Codexshaper\WP\HttpClient;
+
+use Codexshaper\WP\HttpClient\Request;
+use Codexshaper\WP\HttpClient\Response;
+
+/**
+ * REST API HTTP Client Exception class.
+ *
+ * @package Codexshaper/WP
+ */
+class HttpClientException extends \Exception
+{
+    /**
+     * Request.
+     *
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * Response.
+     *
+     * @var Response
+     */
+    private $response;
+
+    /**
+     * Initialize exception.
+     *
+     * @param string   $message  Error message.
+     * @param int      $code     Error code.
+     * @param Request  $request  Request data.
+     * @param Response $response Response data.
+     */
+    public function __construct($message, $code, Request $request, Response $response)
+    {
+        parent::__construct($message, $code);
+
+        $this->request  = $request;
+        $this->response = $response;
+    }
+
+    /**
+     * Get request data.
+     *
+     * @return Request
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Get response data.
+     *
+     * @return Response
+     */
+    public function getResponse()
+    {
+        return $this->response;
+    }
+}
